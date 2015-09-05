fae119be694f4962fa1c1dd95fef4ff24e13ba5f
Danton19 <nicobenito10@gmail.com>
2015-09-05 00:49:11 -0300

    test01

---
+++ project/IComeInPeace/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform target;
+	public float yOffset = 0;
+	
+	
+	void Update() 
+	{
+		this.transform.position = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
+	}
+}
---
+++ project/IComeInPeace/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public float speed = 5f;
+	Rigidbody2D playerBody;
+	Animator playerAnim;
+	void Awake()
+	{
+		playerBody = GetComponent<Rigidbody2D> ();
+		playerAnim = GetComponent<Animator> ();
+	}
+	// Use this for initialization
+	private void PlayerlMovement()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		playerAnim.SetFloat ("HDirection",moveHorizontal);
+		playerAnim.SetFloat ("YDirection",moveVertical);
+		playerBody.velocity = movement * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		PlayerlMovement ();
+	}
+}
