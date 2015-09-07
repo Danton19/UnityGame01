0610befab17a49986f7bdb1434b76dcce62ba166
Danton19 <nicobenito10@gmail.com>
2015-09-07 16:34:21 -0300

    lifebars on enemies

--- project/IComeInPeace/Assets/Scripts/EnemyAI01.cs
+++ project/IComeInPeace/Assets/Scripts/EnemyAI01.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyAI01 : MonoBehaviour {
 
 	public int enemyLife = 40;
+	int totalLife = 40;
 	public GameObject EnemyBullet;
 	Transform target;
 	public int moveSpeed = 100;
@@ -12,21 +13,34 @@ public class EnemyAI01 : MonoBehaviour {
 	Rigidbody2D thisBody;
 	float fireRate = 2f;
 	float nextShoot = 0;
+	Animator enemyAnimator;
+	public GameObject healthBar;
+	GameObject healthBarInstance;
+
 
 	void Awake()
 	{
 		thisBody = GetComponent<Rigidbody2D> ();
 		thisTransform = GetComponent<Transform>();
+		enemyAnimator = GetComponent<Animator> ();
 	}
 	
 	void Start()
 	{
-		target = GameObject.Find ("PlayerTest01").GetComponent<Transform>();		
+		target = GameObject.Find ("PlayerTest01").GetComponent<Transform>();
+		healthBarInstance = Instantiate (healthBar,transform.position,transform.rotation) as GameObject;
+		healthBarInstance.transform.SetParent (GameObject.Find("Canvas").GetComponent<Transform>());
+		healthBarInstance.GetComponent<HealthBarBasic>().targetObject = gameObject.transform;
 	}
 	void ApplyDamage(int damage)
 	{
 		enemyLife -= damage;
-		Destroy (gameObject);
+		float lifePer = (enemyLife*100)/totalLife;
+		healthBarInstance.GetComponent<HealthBarBasic> ().ResizeBar (lifePer);
+		if (enemyLife <= 0) {
+			Destroy (gameObject);
+			Destroy(healthBarInstance);
+		}
 	}
 	void AttackOnPlayer()
 	{
@@ -39,18 +53,33 @@ public class EnemyAI01 : MonoBehaviour {
 			go.GetComponent<Rigidbody2D>().AddForce(toTarget * 100, ForceMode2D.Force);
 		}
 	}
+	void DirectionAnim(Vector3 direction)
+	{
+		if (Mathf.Abs (direction.x) < Mathf.Abs (direction.y)) {
+			enemyAnimator.SetFloat ("HDirection", 0);
+			enemyAnimator.SetFloat ("YDirection", direction.y);
+		} else {
+			enemyAnimator.SetFloat ("YDirection", 0);
+			enemyAnimator.SetFloat ("HDirection", direction.x);
+		}
+	}
+	void EnemyUITasks()
+	{
+
+	}
 	void Update () 
 	{
 		if (target) {
 			if (Vector3.Distance (target.position, thisTransform.position) > range) {
 				Vector3 dir = target.position - thisTransform.position;
 				dir.Normalize ();
-				//Set HDir and YDir
+				DirectionAnim(dir);
 				thisBody.velocity = dir * moveSpeed;
 			} else {
 				AttackOnPlayer ();
 				thisBody.velocity = new Vector3 (0, 0);
 			}
 		}
+		EnemyUITasks ();
 	}
 }
---
+++ project/IComeInPeace/Assets/Scripts/HealthBarBasic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarBasic : MonoBehaviour {
+
+	public Transform targetObject;
+	RectTransform rt;
+	float initWidth;
+
+	void Start()
+	{
+		rt = gameObject.GetComponent<RectTransform> ();
+		initWidth = rt.rect.width;
+	}
+
+	public void ResizeBar(float lifeValue)
+	{
+		float newW = (initWidth / 100) * lifeValue;
+		rt.sizeDelta = new Vector2( newW, rt.rect.height);
+	}
+	void Update ()
+	{
+		if (targetObject) {
+			Vector3 wantedPos =  Camera.main.WorldToScreenPoint (targetObject.position);
+			transform.position = new Vector3(wantedPos.x-15f, wantedPos.y, wantedPos.z);//-15f ajuste para centrar sobre personaje, necesita calcularse
+		}
+	}
+}
